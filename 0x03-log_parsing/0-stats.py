b2216066c6870f2e35353d72055a3da5d903bdb6
AKEEM OLAYINKA AKANNI <100510532+acanite@users.noreply.github.com>
2023-10-19 00:05:59 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,56 +1,40 @@
 #!/usr/bin/python3
+"""0-stats module
 """
-A script: Reads standard input line by line and computes metrics
-"""
+import sys
 
 
-def parseLogs():
+stats = {
+    '200': 0, '301': 0, '400': 0, '401': 0,
+    '403': 0, '404': 0, '405': 0, '500': 0
+}
+total = 0
+count = 0
+
+
+def print_stats(stats, total):
+    """print_stats function
     """
-    Reads logs from standard input and generates reports
-    Reports:
-        * Prints log size after reading every 10 lines & at KeyboardInterrupt
-    Raises:
-        KeyboardInterrupt (Exception): handles this exception and raises it
-    """
-    stdin = __import__('sys').stdin
-    lineNumber = 0
-    fileSize = 0
-    statusCodes = {}
-    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    print("File size: {}".format(total))
+    for key, value in sorted(stats.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
     try:
-        for line in stdin:
-            lineNumber += 1
-            line = line.split()
-            try:
-                fileSize += int(line[-1])
-                if line[-2] in codes:
-                    try:
-                        statusCodes[line[-2]] += 1
-                    except KeyError:
-                        statusCodes[line[-2]] = 1
-            except (IndexError, ValueError):
-                pass
-            if lineNumber == 10:
-                report(fileSize, statusCodes)
-                lineNumber = 0
-        report(fileSize, statusCodes)
-    except KeyboardInterrupt as e:
-        report(fileSize, statusCodes)
-        raise
-
-
-def report(fileSize, statusCodes):
-    """
-    Prints generated report to standard output
-    Args:
-        fileSize (int): total log size after every 10 successfully read line
-        statusCodes (dict): dictionary of status codes and counts
-    """
-    print("File size: {}".format(fileSize))
-    for key, value in sorted(statusCodes.items()):
-        print("{}: {}".format(key, value))
-
-
-if __name__ == '__main__':
-    parseLogs()
-         
+        for line in sys.stdin:
+            data = line.split()
+            if len(data) > 4:
+                status = data[-2]
+                if status in stats.keys():
+                    stats[status] += 1
+                total += int(data[-1])
+                count += 1
+            if count == 10:
+                count = 0
+                print_stats(stats, total)
+    except Exception:
+        pass
+    finally:
+        print_stats(stats, total)
